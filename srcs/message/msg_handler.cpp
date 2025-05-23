e5bada14b0f5f181e54332de46068883c379b808
carlosinti_94@hotmail.com <carlosinti_94@hotmail.com>
2025-05-23 11:55:19 +0200

    small fix include

--- srcs/message/msg_handler.cpp
+++ srcs/message/msg_handler.cpp
@@ -6,11 +6,11 @@
 /*   By: cagonzal <cagonzal@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 22:39:07 by p                 #+#    #+#             */
-/*   Updated: 2025/05/23 11:45:24 by cagonzal         ###   ########.fr       */
+/*   Updated: 2025/05/23 11:55:07 by cagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/message/msg_handler.hpp"
+#include "../include/msg_handler.hpp"
 
 msg_andler::msg_andler() {}
 
