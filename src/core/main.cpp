ee1f94144be385a662fff9940a2cce6495d36baf
p <p@p.com>
2025-10-09 11:43:26 +0200

    añadido lectura puertoy  contraseña del servidor

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -13,9 +13,27 @@
 #include <iostream>
 #include <cstdio>
 #include "Server.hpp"
+#include <cstdlib> // atoi
+#include <string>
 
-int	main(void)
+int	read_args(char** argv)
 {
+	int	port;
+	port = atoi(argv[1]);
+	if (port < 1024 || port > 65535)
+	{
+		std::cerr << "Invalid port number. Please use a port between 1024 and 65535." << std::endl;
+		return 1;
+	}
+	std::cout << "Using port: " << port << std::endl;
+	std::string password = argv[2];
+	std::cout << "Using password: " << password << std::endl;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3 &&!read_args(argv))
+		return 1;
 	std::cout << "Initiating server." << std::endl;
 	Server server;
 	server.run();
