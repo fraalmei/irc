3098abfb35b8e5300ec28788599119f07a17de94
samartin <samartin@student.42madrid.com>
2025-10-31 14:30:34 +0100

    Channel + User changes

--- include/OChannel.hpp
+++ include/OChannel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   OChannel.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cagonzal <cagonzal@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 20:53:50 by p                 #+#    #+#             */
-/*   Updated: 2025/10/09 14:00:29 by cagonzal         ###   ########.fr       */
+/*   Updated: 2025/10/24 13:58:54 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,21 @@ public:
 	const		std::string &getName() const;
 	const		std::vector<User*> &getMembers() const;
 	const		std::vector<User*> &getAdminMembers() const;
-	
+
 private:
 
-	const std::string	_name;
+	const std::string	_name; // Name as no command to change it after creation
 	std::string			_topic;
-	std::string			_password;
+	std::string			_password; // mode -k sets password to ""
 
 	std::vector<User*>	_members; // List of the user sockets
 	std::vector<User*>	_adminMembers; // List of the user sockets
 
-	unsigned int		_userLimit; // User limit = = No Limit
+	unsigned int		_userLimit; // User limit 0 = No Limit
 
 	// Modes
 	bool				_inviteOnly; // Invite only mode
-	bool				_topicProtected; // Topic protected mode
+	bool				_topicProtected; // Topic protected mode. Any user can change the topic when False
 	
 };
 
