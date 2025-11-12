64ae77b10ed8030a6d5efccaa578c53789a2dbe4
samartin <samartin@student.42madrid.com>
2025-11-12 15:46:54 +0100

    fixing a bit

--- src/core/User.cpp
+++ src/core/User.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   User.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: p <p@student.42.fr>                        +#+  +:+       +#+        */
+/*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 17:28:00 by p                 #+#    #+#             */
-/*   Updated: 2025/11/06 18:26:58 by p                ###   ########.fr       */
+/*   Updated: 2025/11/12 15:41:11 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int User::addToBuffer(const std::string& message)
 {
 	_buffer += message;
 	
-	if (message.empty() || message.rfind('\n\r', 0) != message.size() - 2)
+	if (message.empty() || message.rfind("\r\n", 0) != message.size() - 2)
 		return 0;
 	return _buffer.length();
 }
